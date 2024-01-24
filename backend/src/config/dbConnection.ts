c17757e3e1c64b74965c0211fcb1b5c10ee1ffe9
rakoto <endytolojanahary@gmail.com>
2024-01-24 17:55:22 +0300

    (back) feat: create public homepage for the backend

--- backend/src/config/dbConnection.ts
+++ backend/src/config/dbConnection.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const MONGO_URI = process.env.MONGO_URI;
 
+//Set MONGO_URI to the one used in production
 export const dbConnection = async() =>{
 	try {
 		if (MONGO_URI)
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,20 @@
 import express from 'express';
+import path from 'path';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import { dbConnection } from './config/dbConnection';
+import rootRoute from './routes/root';
 
 const app = express();
 const PORT = process.env.PORT;
 
 dbConnection();
 
+app.use('/',express.static(path.join(__dirname,'/public')));
+
+app.use('/',rootRoute);
+
+//PORT needs to set the value of the one used in production
 mongoose.connection.once('open',()=>{
 	console.log('Connected to mongoDB');
 	app.listen(PORT, ()=>{
